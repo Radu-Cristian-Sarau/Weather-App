02c5e9b55e19524c02b7e0dd4f418d22b57f884e
Radu-Cristian-Sarau <radusarau2004@outlook.com>
2024-07-16 14:30:07 +0300

    Finished product
    ATTENTION: website does not work because I haven't subscribed to the API

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const currentTempElement = document.getElementById('current-temp');
 
 const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
 const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-const { token } = require('./config.json');
+import { token } from './config.json';
 
 setInterval(() => {
     const time = new Date();
@@ -20,8 +20,74 @@ setInterval(() => {
     const minutes = time.getMinutes();
     const ampm = hour >= 12 ? 'PM' : 'AM';
 
-    timeElement.innerHTML = hoursIn12HourFormat + ':' + minutes + ' ' + `<span id="am-pm">${ampm}</span>`;
+    timeElement.innerHTML = (hoursIn12HourFormat < 10 ? '0' + hoursIn12HourFormat : hoursIn12HourFormat) + ':' + (minutes < 10 ? '0' + minutes : minutes) + ' ' + `<span id="am-pm">${ampm}</span>`;
 
     dateElement.innerHTML = days[day] + ', ' + date + ' ' + months[month];
 
 }, 1000);
+
+function getWeatherData() {
+    navigator.geolocation.getCurrentPosition((success) => {
+        let {latitude, longitude} = success.coords;
+        fetch(`https://api.openweathermap.org/data/3.0/onecall?lat=${latitude}&lon=${longitude}&exclude=hourly,minutely&units=metric&appid=${token}`).then(res => res.json()).then(data => {
+            showWeatherData(data);
+        });
+    });
+}
+
+function showWeatherData(data) {
+    let {humidity, pressure, sunrise, sunset, wind_speed} = data.current;
+    
+    timezoneElement.innerHTML = data.timezone;
+    countryElement.innerHTML = data.lat + 'N ' + data.lon + 'E';
+
+    currentWeatherItemsElement.innerHTML =
+    `<div class="weather-item">
+        <div>Humidity</div>
+        <div>${humidity}</div>
+    </div>
+    <div class="weather-item">
+        <div>Pressure</div>
+        <div>${pressure}</div>
+    </div>
+    <div class="weather-item">
+        <div>Wind Speed</div>
+        <div>${wind_speed}</div>
+    </div>
+    <div class="weather-item">
+        <div>Sunrise</div>
+        <div>${window.moment(sunrise * 1000).format('HH:mm a')}</div>
+    </div>
+    <div class="weather-item">
+        <div>Sunset</div>
+        <div>${window.moment(sunset * 1000).format('HH:mm a')}</div>
+    </div>
+    `;
+
+    let otherDayForcast = '';
+    data.daily.forEach((day, index) => {
+        if (index == 0) {
+            currentTempElement.innerHTML = `
+                <img src="https://openweathermap.org/img/wn/${day.weather[0].icon}@4x.png" alt="Weather Icon" class="w-icon">
+                <div class="others">
+                    <div class="day">${window.moment(day.dt * 1000).format('ddd')}</div>
+                    <div class="temp">Night - ${day.temp.night}&#176; C</div>
+                    <div class="temp">Day - ${day.temp.day}&#176; C</div>
+                </div>
+            `
+        } else {
+            otherDayForcast += `
+                <div class="weather-forecast" id="weather-forecast">
+                    <div class="weather-forecast-item">
+                        <div class="day">${window.moment(day.dt * 1000).format('ddd')}</div>
+                        <img src="https://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png" alt="Weather Icon" class="w-icon">
+                        <div class="temp">Night - ${day.temp.night}&#176; C</div>
+                        <div class="temp">Day - ${day.temp.day}&#176; C</div>
+                    </div>
+                </div>
+            `
+        }
+    });
+
+    weatherForecastElement.innerHTML = otherDayForcast;
+}
